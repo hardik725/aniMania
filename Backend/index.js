c8924849e93a004b960b5ecef51780989dde634b
Hardik Pandey <156457305+hardik725@users.noreply.github.com>
2024-12-04 22:37:52 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -33,6 +33,10 @@ app.use("/charac", CharacRouter);
 app.use("/review", AnimeReviewRouter);
 app.use("/mangareview", MangaReviewRouter);
 
+app.get('/', (req,res) => {
+    res.send("Welcome")
+})
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
