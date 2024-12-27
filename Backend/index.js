cc49f6fea4fc37ad676b180c96c4a4118041d4aa
Hardik <hardik.pandey.716@gmail.com>
2024-12-27 17:11:48 +0530

    local host is added in the io

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
   cors: {
-    origin: "https://annmania.netlify.app",
+    origin: ["http://localhost:5173", "https://annmania.netlify.app"],
     methods: ["GET","POST"],
   },
 });
